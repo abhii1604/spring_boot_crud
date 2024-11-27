785071c62d24a771973f163c4f74d27c65e09f20
Abhishek kumar <abhishekwant2@gmail.com>
2024-11-27 18:02:17 +0530

    deploy

--- src/main/java/com/example/Book/controller/BookController.java
+++ src/main/java/com/example/Book/controller/BookController.java
@@ -31,6 +31,7 @@ public class BookController {
     @GetMapping("/{id}")
     public ResponseEntity<?> getBookById(@PathVariable Long id) throws Exception {
         Optional<Book> book = bookService.getBookById(id);
+        System.out.println("hi");
         if (book.isPresent())
         {
             return ResponseEntity.ok().body(book.get());
